3c551acec6b3cd2e18622a196ad5e78d591a4e31
John Keck <robertjkeck2@gmail.com>
2018-03-12 14:36:27 -0400

    updated exchange for Decimal error in price

--- kex/settings.py
+++ kex/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(b2-rdlkgo1&-_-!lw+-+3g!_jx98nj3(801u)h%$3+20kn&n&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
